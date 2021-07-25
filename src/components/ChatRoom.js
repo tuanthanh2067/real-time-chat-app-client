1f4a162e67fec2627c7f72cd4972b45d6d8244a4
tuanthanh2067 <tuanthanh2067@gmail.com>
2021-07-25 18:13:38 -0400

    add message component and update chatroom component

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -1,21 +1,48 @@
-import SocketIOClient from "socket.io-client";
+// import SocketIOClient from "socket.io-client";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { FaPaperPlane } from "react-icons/fa";
+
+import Message from "./Message";
 
 export default function ChatRoom() {
-  const socket = SocketIOClient(process.env.REACT_APP_API_URL);
+  // const socket = SocketIOClient(process.env.REACT_APP_API_URL);
 
   let { id } = useParams();
 
-  useEffect(() => {
-    return () => {
-      socket.disconnect();
-    };
-  });
+  // useEffect(() => {
+  //   return () => {
+  //     socket.disconnect();
+  //   };
+  // });
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
 
   return (
-    <div>
-      <h1>Room Id: {id}</h1>
+    <div className="h-screen w-2/4 text-center overflow-hidden">
+      <div className="h-16 border-2 border-opacity-30 rounded-xl my-3 flex items-center justify-center hover:border-opacity-60">
+        <h1 className="font-bold text-2xl text-gray-200 ">Room id: {id}</h1>
+      </div>
+
+      <div className="w-full h-4/5 border-2 border-opacity-30 rounded-xl my-3 hover:border-opacity-60">
+        <Message />
+      </div>
+
+      <form onSubmit={submitHandler} className="h-16 relative">
+        <input
+          type="text"
+          className="w-full h-full border-2 border-opacity-30 rounded-xl bg-transparent outline-none text-gray-100 pl-3 pr-10 hover:border-opacity-60"
+        />
+
+        <button
+          type="submit"
+          className="absolute top-1/2 right-3 transform -translate-y-1/2"
+        >
+          <FaPaperPlane className="text-gray-100 text-xl" />
+        </button>
+      </form>
     </div>
   );
 }
---
+++ src/components/Message.js
@@ -0,0 +1,12 @@
+export default function Message() {
+  return (
+    <div className="flex my-2 ">
+      <div className="text-gray-100 px-3">
+        <h1 className="font-bold text-left">1asdas564qwe</h1>
+        <h1 className="text-sm bg-gray-800 border border-gray-700 border-opacity-70 rounded-lg px-3 py-2 break-all">
+          Hello everyoneeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee!
+        </h1>
+      </div>
+    </div>
+  );
+}
